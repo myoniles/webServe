cc27c4a5fd60ce70ec9a64fef7ad41f40f774c6b
Michael Yoniles <BLMdnst8@gmail.com>
2018-06-25 15:07:35 -0400

    Before macro meddling

--- webServe.c
+++ webServe.c
@@ -1,3 +1,4 @@
+// A project is worth the amount of packages it requires
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -6,14 +7,22 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <dirent.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
 
 #define PORT 8080
 #define ROOT "root"
+#define PROT "HTTP/1.0"
+#define VERSION "0.1.0"
+
+#define wsockid(str) if (write(sockid, str, strlen(str)) != strlen(str)) exit(1);
 
 /*
 	If by chance you are Thomas, looking for a
- 	way to break this, good luck!
-	This program was written in vim not emacs
+ 	way to break this, best of luck!
+
+	This program was written in vim not emacs.
 */
 
 void errormsg( char* msg, int status, int critical ) {
@@ -23,8 +32,57 @@ void errormsg( char* msg, int status, int critical ) {
 	}
 }
 
-void handle(int sockid, DIR* root) {
-	int maxRequestLength = 512;
+char* getDate(){
+	time_t rawtime;
+	struct tm* timeinfo;
+	char* buff = (char*)calloc(50, sizeof(char));
+
+	time (&rawtime);
+	timeinfo = localtime ( &rawtime );
+	strftime(buff, 50, "%a, %d %b %Y %H:%M:%S %Z", timeinfo);
+	return buff;
+}
+
+void sendHeader(int sockid, char* status, char* contentType){
+	write(sockid, PROT, strlen(PROT));
+	wsockid("");
+	//write(sockid, " ", 1);
+	write(sockid, status, strlen(status));
+	write(sockid, "\r\n", 2);
+	write(sockid, "Server: MikeServe/0.1.0\r\n", 25);
+	write(sockid, "Date: ", 6);
+	char* date = getDate();
+	write(sockid, date, strlen(date));
+	write(sockid, "\r\n", 2);
+	write(sockid, "Content-Language: en_US\r\n", 25);
+	write(sockid, "Content-Type: ", 14);
+	write(sockid, contentType, strlen(contentType));
+	write(sockid, "\r\n", 2);
+	write(sockid, "Connection: close\r\n\r\n\r\n", 23);
+	free(date);
+	return;
+}
+
+void genErrorPage(int sockid, char* error){
+	write(sockid, "<html>\r\n<head>\r\n<title>", 23);
+	write(sockid, error, strlen(error));
+	if (write(sockid, "</title>\r\n</head>\r\n<body>\r\n<h1>\r\n", 33) != 33){ return;}
+	write(sockid, error, strlen(error));
+	write(sockid, "</h1>\r\n<hr>\r\n<p>MikeServe version ", 34);
+	write(sockid,  VERSION, strlen( VERSION ));
+	write(sockid, "</p>\r\n</body></html>\r\n", 22);
+}
+
+void sigintHandler(int sig_num) {
+	signal(SIGINT, sigintHandler);
+	// Using write because its Async safe
+	// Even though this will exit, its just better this way
+	write( 0 , "\nServer Shutting Down . . .\n", 28 );
+	exit(1);
+}
+
+void handle(int sockid) {
+	int maxRequestLength = 2048;
 	char buff[maxRequestLength + 1];
 	int currLength = 0;
 	char currChar = 0;
@@ -32,16 +90,29 @@ void handle(int sockid, DIR* root) {
 
 	// Get the request Store it in buff
 	while (currLength < maxRequestLength && read (sockid, &currChar, 1) > 0){
-		if (currChar == '\n' && prevChar == '\r') {
+		if (currChar == '\r' && prevChar == '\n') {
+			// Finish The message
+			buff[currLength] = '\r';
+			buff[++currLength] = '\n';
+			buff[++currLength] = 0;
 			break;
 		}
-
 		buff[currLength++] = currChar;
 		prevChar = currChar;
 	}
-	buff[--currLength] = 0;
 
-	printf("%s\n", buff);
+	// TODO Process request
+	int method;
+
+	// A wise man once said "We only support GET"
+	if (strncmp(buff, "HECK", 3) == 0){
+		method = 1;
+	} else {
+		// The request looks weird aka its not a get request
+		sendHeader(sockid, "400 Bad Request", "text/html" );
+		genErrorPage (sockid, "400 Bad Request");
+	}
+
 	close(sockid);
 }
 
@@ -51,7 +122,11 @@ int main (){
 	int addrlen = sizeof(address);
 	int sockid;
 	int status;
-	DIR* dir = opendir( ROOT );
+
+	// Set up the sigint handler as to ensure the socket is closed
+
+	signal(SIGINT, sigintHandler);
+
 
 	// Create the socket
  	if ( (sockid = socket(PF_INET, SOCK_STREAM,0))  == 0) {
@@ -73,18 +148,22 @@ int main (){
 		int status = close(sockid);
 	}
 
-	// Listen to on PORT
-	if ((status=listen(sockid, 3) < 0 )){
-		errormsg("I couldn't hear you if I wanted to...", status, 1);
-	}
 
-	int newSoc;
-	// Accept incoming requests
-	if ( (newSoc=accept(sockid, (struct sockaddr*)&address,(socklen_t*)&addrlen )) < 0) {
-		errormsg("I object!", status, 0);
-	}
+	while ( 1 ){
+		// Listen to on PORT
+		if ((status=listen(sockid, 3) < 0 )){
+			errormsg("I couldn't hear you if I wanted to...", status, 1);
+		}
+		
+		int newSoc;
+		// Accept incoming requests
+		if ( (newSoc=accept(sockid, (struct sockaddr*)&address,(socklen_t*)&addrlen )) < 0) {
+			errormsg("I object!", status, 0);
+		}
 
-	handle( newSoc, dir);
+		handle( newSoc ); 
+
+	}
 
 	status = close(sockid);
 
